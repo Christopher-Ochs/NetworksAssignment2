eac6f7f79a33ca0c05f8b17a95f5494958dcf133
Philip Tallo <tallopt@mail.uc.edu>
2018-09-18 11:39:54 -0400

    working Client/Server

---
+++ src/MessagingClient.java
@@ -0,0 +1,24 @@
+/*
+    Name: Phillip Tallo and Christopher Ochs
+    Date: September 18, 2018
+    Desc: Assignment 2 Computer Networks.  Client Server Protocol.
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.util.Arrays;
+
+public class MessagingClient {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("127.0.0.1", 8080);
+
+        InputStream is = socket.getInputStream();
+
+        int size = is.read();
+        byte[] message = new byte[size];
+        is.read(message, 0, size);
+
+        System.out.println(new String(message));
+    }
+}
---
+++ src/MessagingServer.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MessagingServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(8080);
+        Socket socket = server.accept();
+
+        OutputStream os = socket.getOutputStream();
+        InputStream is = socket.getInputStream();
+
+        String message = "Hello Client!!!!";
+
+        byte[] byte_message = message.getBytes();
+
+        os.write(byte_message.length);
+        os.write(byte_message);
+        os.flush();
+    }
+}
